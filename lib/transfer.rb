8f7011415bf11afd605dc50ba18a827da17bb318
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-21 03:34:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,11 +22,12 @@ class Transfer
 
     if @sender.valid? == false
       "Transaction rejected. Please check your account balance."
+
       self.status = "Transaction rejected. Please check your account balance."
 
     else
 
-      @receiver.deposit(@amount)
+      @receiver.balance = @receiver.balance + @amount
       @sender.balance = @sender.balance - @amount
 
       self.status = "complete"
