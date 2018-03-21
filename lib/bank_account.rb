874e390472d8c208ecdf4f77e710963951a209a3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-21 03:08:23 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,7 +18,11 @@ class BankAccount
   end
 
   def valid?
-    false if self.status != "open" && self.balance <= 0
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
   end
 
   def close_account(user)
